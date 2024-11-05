2822841a8452184e5431db0440d41480ec0e5e8b
pear-c <100738513+pear-c@users.noreply.github.com>
2024-11-05 18:21:30 +0900

    [Baekjoon-18258] 큐 2

---
+++ Baekjoon/sunghwan/0_week/B_18258.java
@@ -0,0 +1,62 @@
+package IntStudy;
+
+import java.io.*;
+import java.util.Deque;
+import java.util.LinkedList;
+
+// [백준_18258] 큐 2
+public class B_18258 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        // 저장할 큐
+        Deque<Integer> q = new LinkedList<>();
+
+        int N = Integer.parseInt(br.readLine());
+        while (N-- > 0) { // N번 만큼 반복
+            // 커맨드 입력
+            String command = br.readLine();
+
+            // #1. push (기존의 StringTokenizer 방법에서 변경)
+            if(command.contains("push")){
+                int num = Integer.parseInt(command.split(" ")[1]);
+                q.add(num);
+            }
+            else {
+                switch (command) {
+                    // #2. pop
+                    case "pop":
+                        sb.append(q.isEmpty() ? "-1" : q.poll()).append("\n");
+                        break;
+
+                    // #3. size
+                    case "size":
+                        sb.append(q.size()).append("\n");
+                        break;
+
+                    // #4. empty
+                    case "empty":
+                        sb.append(q.isEmpty() ? "1\n" : "0\n");
+                        break;
+
+                    // #5. front
+                    case "front":
+                        sb.append(q.isEmpty() ? "-1" : q.peek()).append("\n");
+                        break;
+
+                    // #6. back
+                    case "back":
+                        sb.append(q.isEmpty() ? "-1" : q.getLast()).append("\n");
+                        break;
+                }
+            }
+        }
+        bw.write(sb.toString());
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
