aa5ba3d0c4e5fd33c5e2bc3546df8cffbd807394
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-05 14:17:37 +0900

    [Baekjoon-1001] A-B

---
+++ Baekjoon/heemang/week0/A-B.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // num1과 num2를 입력 받는다.
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        
+        // num1 - num2
+        System.out.println(num1 - num2);
+    }
+}
