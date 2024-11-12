3d3a1e014630ca81050460f09cb123d21da79353
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-12 23:19:15 +0900

    [Baekjoon-11728] 배열 합치기
    https://www.acmicpc.net/problem/11728

---
+++ Baekjoon/heemang/2_week/11728.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N, M;
+    static int[] a;
+    static int[] b;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        N = Integer.parseInt(inputs[0]);
+        M = Integer.parseInt(inputs[1]);
+
+        a = new int[N];
+        b = new int[M];
+
+        inputs = br.readLine().split(" ");
+        for (int j = 0; j < N; j++) {
+            a[j] = Integer.parseInt(inputs[j]);
+        }
+        inputs = br.readLine().split(" ");
+        for (int j = 0; j < M; j++) {
+            b[j] = Integer.parseInt(inputs[j]);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int pA = 0, pB = 0;
+        while (pA < N && pB < M) {
+            // 오름차순 출력
+            if (a[pA] < b[pB]) {
+                sb.append(a[pA++] + " ");
+            } else {
+                sb.append(b[pB++] + " ");
+            }
+        }
+
+        // 출력하지 못한 나머지 값들 출력
+        while (pA < N) {
+            sb.append(a[pA++] + " ");
+        }
+        while (pB < M) {
+            sb.append(b[pB++] + " ");
+        }
+
+        System.out.println(sb);
+    }
+}
